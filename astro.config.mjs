a842abb8215f750c440574046f31ea1b24d3f9c7
jclewis33 <109756608+jclewis33@users.noreply.github.com>
2025-04-06 14:14:10 -0500

    Setup Design System

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // This is the URL of your live site. Astro uses this for generating canonical URLs and your sitemap.
+  site: "https://www.caseylewisphotography.com",
+
+  // Optional: If your site is hosted in a subdirectory, specify the base path.
+  // base: '/your-subdirectory/',
+
+  // Optional: Control whether URLs have trailing slashes. Options are "always", "never", or "ignore".
+  // trailingSlash: 'always',
+});
