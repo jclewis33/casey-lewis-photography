27c3f30f86d7c4fb162748b00b8e28c88d755615
jclewis33 <109756608+jclewis33@users.noreply.github.com>
2025-06-13 07:25:53 -0500

    Image optimization google tag manager

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+//import { imageService } from "@unpic/astro/service";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    responsiveImages: true,
+  },
   // This is the URL of your live site. Astro uses this for generating canonical URLs and your sitemap.
   site: "https://www.caseylewisphotography.com",
+
+  image: {
+    experimentalLayout: "constrained",
+    //service: imageService(),
+  },
 
   // Optional: If your site is hosted in a subdirectory, specify the base path.
   // base: '/your-subdirectory/',
